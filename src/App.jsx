ebee329ef949a8c5361ccdc8d6f4f365d9a4d407
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2023-12-28 14:27:34 -0300

    Add a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header () {
+
+  return (
+    <header>
+      <nav>
+        <a href="#">Logo</a>
+        <input type="text" />
+        <a href="#">About</a>
+        <a href="#">Cart</a>
+
+      </nav>
+
+    </header>
+  )
+}
