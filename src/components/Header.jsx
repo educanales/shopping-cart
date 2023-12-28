62fc95bc22d0ff0b3a6c4cf070b5ded37072daa1
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2023-12-28 18:07:55 -0300

    Style Header component with Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
-export default function Header () {
+import { IconDeviceGamepad2, IconShoppingCart } from '@tabler/icons-react';
 
+export default function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Logo</a>
-        <input type="text" />
+      <nav className="flex items-center gap-4 p-4">
+        <a href="#" className="mr-10 ">
+          <IconDeviceGamepad2 className='size-8'/>
+        </a>
+        <input
+          type="text"
+          placeholder="The Witcher 3, GTA V, Mount & Blade, etc"
+          className="w-full h-10 rounded-md pl-2"
+        />
         <a href="#">About</a>
-        <a href="#">Cart</a>
-
+        <a href="#"><IconShoppingCart className='size-8' /></a>
       </nav>
-
     </header>
-  )
+  );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function() {
+
+  return (
+    <section>
+      <div className="w-full h-96 bg-slate-700">
+        <h1 className="text-white">Popular now</h1>
+      </div>
+    </section>
+  )
+}
